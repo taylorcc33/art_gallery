97d3200fa0ab00911f967dd23f05285ad3a063f1
Taylor Collins <taylorcc33@gmail.com>
2020-11-30 14:20:12 -0700

    refactored script

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
+// Existing artworks array
 
-let user1 = {name:'Tony', ranking:2100};
-let user2 = {name:'Sally', ranking: 1100};
 
-// state - current state of data
-let state = {
-  headerText: 'Users',
-  users: [user1, user2],
-  sortBy: 'ranking', 
-};
 
-const renderUsers = () => {
-  return `<div>users here</div>`
-};
+// STATE
 
-// render function - takes state and rendering it to DOM
-const render = () => {
-  console.log("render called");
-  let root = document.getElementById("root");
-  let htmlString = `<h1>${state.headerText}</h1>`;
-
-  htmlString += renderUsers();
-  root.innerHTML = htmlString;
-  
-};
-
-// initial render
-render();
-
-console.log('script loaded');
