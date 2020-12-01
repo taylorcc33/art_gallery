126d2ab9627e1cd6904f41db6622fb7858cd1846
Taylor Collins <taylorcc33@gmail.com>
2020-11-30 18:05:29 -0700

    updated ui css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Existing artworks array
-let book1 = { bookCover: `<img src="https://64.media.tumblr.com/ddbe258c372fc772cf22053494e1a0ab/tumblr_oei29uHFSd1vnd7blo4_500.png" width = "230">`, author: "Tender B. Cryin", title: "Save us, Son - If I Die I Go to Hell" };
-let book2 = { bookCover: `<img src="https://64.media.tumblr.com/a8958755f56511e6cd57b1c2b8bcd27a/tumblr_oehwwz6C6v1vnd7blo1_400.png" width = "230">`, author: "Maureen Daly", title: "He Just Ate a Whole Bag of Cool Ranch Doritos" };
-let book3 = { bookCover: `<img src="https://pbs.twimg.com/media/ElHG9JnU0AEJGuN?format=jpg&name=900x900" width = "230">`, author: "Jeff Hurting", title: "Here They Come, the Bats that Kicked My Ass" };
+let book1 = { bookCover: `<img src="https://64.media.tumblr.com/ddbe258c372fc772cf22053494e1a0ab/tumblr_oei29uHFSd1vnd7blo4_500.png" height = "350">`, author: "Tender B. Cryin", title: "Save us, Son - If I Die I Go to Hell" };
+let book2 = { bookCover: `<img src="https://64.media.tumblr.com/a8958755f56511e6cd57b1c2b8bcd27a/tumblr_oehwwz6C6v1vnd7blo1_400.png" height = "350">`, author: "Maureen Daly", title: "He Just Ate a Whole Bag of Cool Ranch Doritos" };
+let book3 = { bookCover: `<img src="https://pbs.twimg.com/media/ElHG9JnU0AEJGuN?format=jpg&name=900x900" height = "350">`, author: "Jeff Hurting", title: "Here They Come, the Bats that Kicked My Ass" };
 
 // STATE
 let state = {
@@ -13,10 +13,12 @@ let state = {
 // Add book
 const addBook = () => {
   console.log("add book here");
+  let bookCoverUrl = prompt("enter book cover url");
+  bookCover = `<img src="${bookCoverUrl}" width="230">`
   let author = prompt("enter author name");
   let title = prompt("enter title");
 
-  let newBook = {author: author, title: title };
+  let newBook = {author: author, title: title, bookCover: bookCover };
   state.books.push(newBook);
   render();
 };
@@ -52,12 +54,12 @@ const deleteBook = (index) => {
 
 // Render
 const renderBooks = () => {
-  let htmlString = `<div class='list'>`;
+  let htmlString = `<div class='list row'>`;
   state.books.forEach((book, index) => {
-    htmlString += `<div class='book'>
-                    <div onclick='updateBook(${index})'>${book.bookCover}<br /> Author:&nbsp ${book.author}<br /> Title:&nbsp ${book.title}</div>
-                    <button onclick='deleteBook(${index})'>delete</button>
-                   </div><br />`
+    htmlString += `<div class='book col s12 m6 l4'>
+                    <div onclick='updateBook(${index})'>${book.bookCover}<br /> <b>Author:</b>  ${book.author}<br /> <b>Title:</b>  ${book.title}</div>
+                    <a class='waves-effect red lighten-2 btn-small' onclick='deleteBook(${index})'>delete</a>
+                   </div>`
   });
   htmlString += `</div>`;
   return htmlString;
@@ -66,8 +68,8 @@ const renderBooks = () => {
 const render = () => {
   console.log("render called");
   let root = document.getElementById("root");
-  let htmlString = `<h1>${state.headerText}</h1>`;
-  htmlString += `<button id='add' onclick='addBook()'>add Book</button><br /><br />`;
+  let htmlString = `<h1>${state.headerText}</h1><h4>The Worst Bookstore on Earth - welcome to the nightmare</h4><br /><hr />`;
+  htmlString += `<a class='waves-effect waves-light btn' id='add' onclick='addBook()'>add Book</a><br /><br />`;
   htmlString += renderBooks();
   root.innerHTML = htmlString;
 };
